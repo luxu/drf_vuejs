5dd8afe585d3d01c307a0eb396373c6865eac656
Luciano Martins <zicadopv@gmail.com>
2021-07-17 07:15:23 -0300

    Iniciando o frontend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <q-separator/>
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn
+          color="black"
+          label="Incluir Preso"
+          size="xl"
+          to="/add"
+      />
+      <q-btn
+          color="black"
+          label="Excluir Preso"
+          size="xl"
+          to="/delete"
+      />
+    </div>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
